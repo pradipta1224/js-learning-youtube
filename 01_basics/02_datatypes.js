6a4bdfb36d5152bb57aadcd1603462a48c1b0567
pradipta1224 <113927963+pradipta1224@users.noreply.github.com>
2024-09-03 11:48:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //it treats all the js code as a newer version although it's not mandatory nowadays
+
+// alert(3 + 3) we are using nodejs and not browser
+
+let name = "Pradipta" // String
+let age = 19 //Integer
+let isLoggedIn = true // Boolean
+
+//number => 2 to the power 53 - Range
+//bigInt
+//string => ""
+//boolean => true/false
+//null => Standalone value as well and not only a datatype, can be assigned as null also to represent an empty datatype
+//undefined => value not assigned to the datatype
+//symbol => used to show uniqueness
+
+
+//object =>
+
+//There is a type of keyword in javascript and that is typeOf to get to know the datatype and when you type in typeOf null then it shows object as the output 
