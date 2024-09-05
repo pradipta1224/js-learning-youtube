7488d21245142b7d60139bea8023d620fbe26a28
pradipta1224 <113927963+pradipta1224@users.noreply.github.com>
2024-09-05 05:53:18 +0000

    done

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -21,4 +21,21 @@ console.log(hundreds.toLocaleString('en-IN')); //Mainly used in e-commerce websi
 //This was all about numbers, now begins MATHS
 //Maths is a library in Javascript which comes with it by default
 
-console.log(Math)
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
