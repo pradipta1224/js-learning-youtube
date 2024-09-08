67e594d21be8d3aa0d9ca2e70887299fd160ef89
pradipta1224 <113927963+pradipta1224@users.noreply.github.com>
2024-09-08 18:51:23 +0000

    functions commited

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,31 @@
+function sayMyName(){
+    console.log("Repition is the key")
+}
+
+//This is the basic definition of the function
+
+// sayMyName //This is the reference of the function
+// sayMyName() //This is the execution of the function
+
+// function addTwoNumbers(a,b) { //a and b are the parameters
+//      console.log(a+b)
+// }
+
+// addTwoNumbers(3,4) //Calling the no.
+//These 3 and 4 are the arguments to the parameters a and b inside the parenthesis of addTwoNumbers
+//Parameters are the inputs of the functions, and the arguments are the values of those parameters
+
+const result = addTwoNumbers(3,5)
+console.log("Result value is ", result) //Output is undefined when it comes to result's value
+
+function addTwoNumbers(a,b) { //a and b are the parameters
+    let result = a + b
+    console.log("Pradipta")
+    return result
+    // console.log("Pradipta") //It won't print coz nothing prints after the keyword result is used
+}
+
+function loginUserMessage(username){
+    return `${username} just logged in`  //Important
+}
+console.log(loginUserMessage("Pradipta")) //When you don't pass anything as an argument then it returns undefined 
