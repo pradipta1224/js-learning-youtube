eabe34e7f98ad12cc3c711c82d47878b788e0b8b
pradipta1224 <113927963+pradipta1224@users.noreply.github.com>
2024-09-09 10:51:50 +0000

    scope comitted

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,19 @@
+
+
+
+if(true){
+    let a = 10
+const b = 20
+var c = 30
+} //This basically is the scope, it's not the scope when it's object, that's different, that's object declaration, but when it's functions or nested if else or loops then it's scope
+
+
+
+// console.log(a) //An error shows which says a is not defined coz it's inside the braces, inside the scope
+// console.log(b) //Same with b, just like a
+console.log(c) //But when we comment out a and b console.logs then we can see that the output of c shows up and prints 30 , coz the datatype used here is var and that's the fault of using var, it doesn't obey the laws of block scope or local scope
+
+
+//Global scope is different in the browser console vs the code envinronment
+
+
