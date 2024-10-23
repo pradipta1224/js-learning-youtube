d49640d0c25f1284ea245ada5e20b8f5d4befd89
pradipta1224 <113927963+pradipta1224@users.noreply.github.com>
2024-10-23 18:27:53 +0000

    changes comitted

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -3,11 +3,22 @@ const myNumbers = [1,2,3,4,5,6,7,8,9,10]
 // const newNums = myNumbers.map( (num)=>{return num+10})//Also automatically returns value
 
 
-const newNums = myNumbers
-.map((num)=> num*10)
-.map((num)=> num+1)
-.filter((num)=> num > 30)
- //This is called chaining
+// const newNums = myNumbers
+// .map((num)=> num*10)
+// .map((num)=> num+1)
+// .filter((num)=> num > 30)
+//  This is called chaining
+
+const myNum = myNumbers
+  .map(num => num * 10)    
+  .map(num => num + 1)
+  .map((num, index) => ({ num, index })) 
+  .filter(item => item.num > 30)         
+  .map(item => item.index);      
 
 
-console.log(newNums)
+
+
+console.log(myNum)
+
+
